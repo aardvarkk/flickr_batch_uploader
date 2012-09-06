f28de6a66d9d397813b3037ce4e6d5e6feb56dd8
Ian Clarkson <clarksonian@gmail.com>
2012-09-05 22:33:48 -0400

    Basic command line parsing using Trollop to get the root folder.

--- flickr_batch_uploader.rb
+++ flickr_batch_uploader.rb
@@ -1,7 +1,11 @@
-require 'flickraw'
+require 'flickraw' # flickr API
+require 'trollop' # command line parsing
 
 FlickRaw.api_key = '2ce063626721827c2f7983a402ed16da'
-FlickRaw.shared_secret = File.open('secret', 'r').read
+FlickRaw.shared_secret = File.read('secret')
 
-puts FlickRaw.api_key
-puts FlickRaw.shared_secret
+opts = Trollop::options do
+  opt :root, 'Root directory', :type => :string
+end
+
+p opts
